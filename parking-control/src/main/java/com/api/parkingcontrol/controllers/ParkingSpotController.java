bd9b210f93bab56e7786a5f0e681706fccb0cccf
LuisaGPurificacao <luisa.purificacao16@gmail.com>
2023-01-10 15:50:53 -0300

    POST de Parking Lot

--- parking-control/src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ parking-control/src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -1,10 +1,17 @@
 package com.api.parkingcontrol.controllers;
 
+import com.api.parkingcontrol.dtos.ParkingSpotDto;
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.services.ParkingSpotService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -13,5 +20,19 @@ public class ParkingSpotController {
 
     @Autowired
     private ParkingSpotService service;
+
+    @PostMapping
+    public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto dto) {
+        if (service.existsByLicensePlateCar(dto.getLicensePlateCar()))
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: License Plate Car is already in use!");
+        if (service.existsByParkingSpotNumber(dto.getParkingSpotNumber()))
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot is already in use!");
+        if (service.existsByApartmentAndBlock(dto.getApartment(), dto.getBlock()))
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot already registered for this apartment/block!");
+        ParkingSpotModel model = new ParkingSpotModel();
+        BeanUtils.copyProperties(dto, model);
+        model.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(model));
+    }
 
 }
--- parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,7 +1,9 @@
 package com.api.parkingcontrol.services;
 
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ParkingSpotService {
@@ -10,5 +12,22 @@ public class ParkingSpotService {
 
     public ParkingSpotService(ParkingSpotRepository repository) {
         this.repository = repository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel model) {
+        return repository.save(model);
+    }
+
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return repository.existsByLicensePlateCar(licensePlateCar);
+    }
+
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return repository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return repository.existsByApartmentAndBlock(apartment, block);
     }
 }
